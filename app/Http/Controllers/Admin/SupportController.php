f8358482bdd4433a0e8295cf709de67fdd10ab89
Luciano <61517478+devluciano@users.noreply.github.com>
2024-02-13 15:37:27 -0300

    view
    criando a View suporte

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -15,4 +15,18 @@ class SupportController extends Controller
 
         return view("admin.supports.index", compact("supports"));
     }
+
+    public function create()
+    {
+        return view("admin.supports.create");
+    }
+
+    public function store(Request $request, Support $support) 
+    {
+        $data = $request->all();
+        $data['status'] = 'a';
+
+        $support->create($data);
+        return redirect()->route('supports.index');
+    }
 }
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,24 @@
 <h1>Listagem dos Suportes</h1>
+
+<a href="{{ route('supports.create')}}">Criar Dúvida</a>
+
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>Status</th>
+        <th>Descrição</th>
+        <th></th>
+    </thead>
+    <tbody>
+        @foreach ($supports as $support )
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td>
+                    >
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
